ba3ed35d7007c3143219ec5e8047fb0db6652a90
riverdev <yariv.rivercybe@gmail.com>
2024-09-17 21:13:14 +0300

    Added a basic table component, at this stage it just uses a temporary data array, I will later connect it to the firm data

--- src/app/assets-table/assets-table.component.ts
+++ src/app/assets-table/assets-table.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-assets-table',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './assets-table.component.html',
-  styleUrl: './assets-table.component.css'
+  styleUrl: './assets-table.component.css',
 })
 export class AssetsTableComponent {
+  dataSource = [
+    {
+      id: 1,
+      name: 'aaa',
+    },
+    {
+      id: 2,
+      name: 'bbb',
+    },
+    {
+      id: 3,
+      name: 'ccc',
+    },
+  ];
 
+  displayedColumns: string[] = ['id', 'name'];
 }
