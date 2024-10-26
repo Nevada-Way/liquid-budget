0d9b8d33feb59287273905db0cd143c0c3ecc80a
riverdev <yariv.rivercybe@gmail.com>
2024-10-26 13:48:54 +0300

    Did some code cleanup and organize

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Link to the GitHub project:
 https://github.com/Nevada-Way/liquid-budget.git
+
+Link to the netlify deployed app :
+https://6713ab45a207c8aae7293347--our-budget1.netlify.app/
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, signal, WritableSignal } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 import { Firm } from '../interfaces/finance.model';
 import { FinancialService } from '../services/financial.service';
@@ -6,9 +7,6 @@ import { AssetsTableComponent } from '../assets-table/assets-table.component';
 import { ChartComponent } from '../chart/chart.component';
 import { HelperService } from '../services/helper.service';
 import { FormsModule } from '@angular/forms';
-
-// declare var google: any;
-// import * as google from 'google-charts';
 
 @Component({
   selector: 'app-home',
@@ -18,9 +16,11 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
+  signalTotalFirmBalance: WritableSignal<number> = signal(100000); //709000
+
   firms: Firm[] = [];
+
   charts: any[] = []; // Array to store charts
-  signalTotalFirmBalance: WritableSignal<number> = signal(709000);
   totalFirmBalance: number = this.signalTotalFirmBalance();
 
   constructor(
@@ -30,7 +30,6 @@ export class HomeComponent implements OnInit {
 
   async ngOnInit() {
     try {
-      //const totalFirmBalance: number = this.signalTotalFirmBalance();
       const generatedFirms: Firm[] = this.FinancialService.generateDemoFirms();
       console.log('Firms fetched:', generatedFirms);
 
@@ -38,10 +37,6 @@ export class HomeComponent implements OnInit {
         generatedFirms,
         this.totalFirmBalance
       );
-
-      // console.log('Actual value firms:', this.firms);
-
-      // this.FinancialService.test(this.firms[0].accounts);
     } catch (error) {
       console.error('Error fetching firms:', error);
     }
